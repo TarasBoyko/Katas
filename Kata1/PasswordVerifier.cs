022bf3f665f83555a930cc92e1d28ca0692b1417
Boyko Taras <taras.boyko2@gmail.com>
2018-12-14 19:09:03 +0200

    first point of the third kata

---
+++ Kata1/PasswordVerifier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata1
+{
+    // A class for password verifications.
+    class PasswordVerifier
+    {
+        public bool Verify(string password)
+        {
+            if (password == null)
+            {
+                throw new ArgumentNullException("password", "password should not be null");
+            }
+
+            const int passwordMinLength = 9;
+            if (password.Length < passwordMinLength)
+            {
+                throw new ArgumentException("password should be larger than 8 chars");
+            }
+
+            try
+            {
+                password.First((c) => Char.IsLower(c));              
+            }
+            catch(InvalidOperationException)
+            {
+                throw new ArgumentException("password should have one lowercase letter at least");
+            }
+            try
+            {
+                password.First((c) => Char.IsUpper(c));
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ArgumentException("password should have one uppercase letter at least");
+            }
+            try
+            {
+                password.First((c) => Char.IsDigit(c));
+            }
+            catch (InvalidOperationException)
+            {
+                throw new ArgumentException("password should have one number at least");
+            }
+           
+            return true;
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata1;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class PasswordVerifier_Test
+    {
+        [TestMethod]
+        public void Verify_PassNull_ThrowNullArgumentException()
+        {
+            PasswordVerifier verifier = new PasswordVerifier();
+
+            try
+            {
+                verifier.Verify(null);
+            }
+            catch (ArgumentNullException e)
+            {
+                Assert.AreEqual("password should not be null" + Environment.NewLine + "Parameter name: password", e.Message);
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("the thrown exception is not ArgumentNullException");
+            }
+         
+            Assert.Fail("exception is not thrown");
+        }
+
+        [TestMethod]
+        public void Verify_PassArgWithLenghtLessThanMin_ThrowArgumentException()
+        {
+            PasswordVerifier verifier = new PasswordVerifier();
+
+            try
+            {
+                verifier.Verify("Uj3hdf78");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("password should be larger than 8 chars", e.Message);
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("the thrown exception is not ArgumentException");
+            }
+
+            Assert.Fail("exception is not thrown");
+        }
+
+        [TestMethod]
+        public void Verify_PassLowerLessArg_ThrowArgumentException()
+        {
+            PasswordVerifier verifier = new PasswordVerifier();
+
+            try
+            {
+                verifier.Verify("123456345BG");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("password should have one lowercase letter at least", e.Message);
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("the thrown exception is not ArgumentException");
+            }
+
+            Assert.Fail("exception is not thrown");
+        }
+
+        [TestMethod]
+        public void Verify_PassUpperLessArg_ThrowArgumentException()
+        {
+            PasswordVerifier verifier = new PasswordVerifier();
+
+            try
+            {
+                verifier.Verify("12345dfs6345");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("password should have one uppercase letter at least", e.Message);
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("the thrown exception is not ArgumentException");
+            }
+
+            Assert.Fail("exception is not thrown");
+        }
+
+        [TestMethod]
+        public void Verify_PassNumberLessArg_ThrowArgumentException()
+        {
+            PasswordVerifier verifier = new PasswordVerifier();
+
+            try
+            {
+                verifier.Verify("hfdskfhJHHFjfdsheu");
+            }
+            catch (ArgumentException e)
+            {
+                Assert.AreEqual("password should have one number at least", e.Message);
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("the thrown exception is not ArgumentException");
+            }
+
+            Assert.Fail("exception is not thrown");
+        }
+    }
+}
